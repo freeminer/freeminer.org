328d14f943f4053e35ef057f799f9fccdc32e365
Ilya Zhuravlev <zhuravlevilya@ya.ru>
2013-11-27 17:27:31 +0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+http://freeminer.org/ website.
---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, redirect
+from flask.ext.assets import Environment, Bundle
+from jinja2.exceptions import TemplateNotFound
+
+app = Flask(__name__)
+assets = Environment(app)
+
+css = Bundle(
+    "css/reset.css",
+    "css/style.css",
+    filters="cssmin",
+    output="gen/packed.css"
+)
+
+assets.register('css_all', css)
+
+@app.route("/<page>/")
+def show_page(page):
+    try:
+        return render_template("pages/{}.html".format(page), page=page)
+    except TemplateNotFound:
+        return redirect("/")
+
+@app.route("/")
+def index():
+    return show_page("index")
+
+if __name__ == "__main__":
+    app.run(debug=True)
